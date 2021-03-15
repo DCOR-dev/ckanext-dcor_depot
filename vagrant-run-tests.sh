b9d6c7a9a3ddcdc0deb14f19390500b79e565b44
Paul Müller <dev@craban.de>
2021-03-15 23:51:43 +0100

    tests: setup CI with vagrant image

---
+++ vagrant-run-tests.sh
@@ -0,0 +1,17 @@
+set -e
+set -x
+# Set CKAN_INI to testing
+export CKAN_INI=/etc/ckan/default/test-dcor.ini
+# Go to the directory of this script
+cd "$(dirname "${BASH_SOURCE[0]}")"
+# Source the CKAN environment
+source /usr/lib/ckan/default/bin/activate
+# Update all DCOR extensions
+dcor update --yes
+# Install the current package in editable mode for testing
+pip install -e .
+# run tests with coverage
+coverage run --source=ckanext.dcor_schemas -m pytest -p no:warnings ckanext
+# Get GitHub environment variables so codecov detects GH Actions (allow command to fail)
+export $(grep -v '^#' environment.txt | xargs) || exit 0
+codecov
