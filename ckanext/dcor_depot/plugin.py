a0e32647efe3016dbe96abcffeaa6683848b73c1
Paul Müller <dev@craban.de>
2024-11-25 12:23:29 +0100

    fix: make public resources public on S3 only after dataset is active

--- ckanext/dcor_depot/plugin.py
+++ ckanext/dcor_depot/plugin.py
@@ -20,8 +20,12 @@ class DCORDepotPlugin(plugins.SingletonPlugin):
 
     # IPackageController
     def after_dataset_update(self, context, data_dict):
+        # This method is called *after* the dataset has been updated.
         private = data_dict.get("private")
-        if private is not None and not private:
+        if (private is not None and not private
+                # Only do this for "active" datasets, otherwise this will
+                # be run after *every* resource update (package_revise).
+                and data_dict["state"] == "active"):
             # Normally, we would only get here if the user specified the
             # "private" key in `data_dict`. Thus, it is not an overhead
             # for normal operations.
@@ -29,10 +33,8 @@ class DCORDepotPlugin(plugins.SingletonPlugin):
             # dataset has been private before. If we already have resources
             # in this dataset, then we have to set the S3 object tag
             # "public:true", so everyone can access it.
-            orig_dict = toolkit.get_action("package_show")(
-                context=context, data_dict={"id": data_dict["id"]})
             # Make sure the S3 resources get the "public:true" tag.
-            for res in orig_dict["resources"]:
+            for res in data_dict["resources"]:
                 s3cc.make_resource_public(res["id"])
 
     # IResourceController
