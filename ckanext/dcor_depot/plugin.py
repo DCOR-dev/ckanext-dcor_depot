983481aec13ade6e1be6f321de0ad2a9e6aa3abf
Paul Müller <dev@craban.de>
2025-07-13 00:43:58 +0200

    fix: plugin did not implement `IConfigDeclaration`

--- ckanext/dcor_depot/plugin.py
+++ ckanext/dcor_depot/plugin.py
@@ -1,3 +1,4 @@
+from ckan import config
 import ckan.plugins as plugins
 
 from dcor_shared import s3
@@ -8,11 +9,24 @@ from . import jobs
 
 class DCORDepotPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IClick, inherit=True)
+    plugins.implements(plugins.IConfigDeclaration, inherit=True)
     plugins.implements(plugins.IResourceController, inherit=True)
 
     # IClick
     def get_commands(self):
         return get_commands()
+
+    # IConfigDeclaration
+    def declare_config_options(
+            self,
+            declaration: config.declaration.Declaration,
+            key: config.declaration.Key):
+
+        dcor_depot_group = key.ckanext.dcor_depot
+
+        declaration.declare(dcor_depot_group.tmp_dir).set_description(
+            "temporary directory for importing resource files"
+        )
 
     # IResourceController
     def after_resource_create(self, context, resource):
