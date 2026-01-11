5b1adb68a545302a4ebd0c429f6413fe0643ad67
Paul Müller <dev@craban.de>
2026-01-11 23:31:56 +0100

    disable compression for figshare

--- ckanext/dcor_depot/figshare.py
+++ ckanext/dcor_depot/figshare.py
@@ -93,7 +93,6 @@ def import_dataset(doi):
         "Accept": "text/html,application/xhtml+xml,application/xml;"
                   "q=0.9,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
         "DNT": "1",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
